0037636e9cf4e6309b5aeb6de5c72445f3d779c2
abalozz <alivanprox@gmail.com>
2015-04-29 00:13:51 +0200

    La hora de envío del mensaje se actualiza automáticamente

--- public/chat.jsx
+++ public/chat.jsx
@@ -102,20 +102,51 @@ var Message = React.createClass({
 
   render: function () {
     return (
-      <li className="message"><strong>{this.props.name}:</strong> {this.props.children} <TimeAgo>{this.props.time}</TimeAgo></li>
+      <li className="message"><strong>{this.props.name}:</strong> {this.props.children} <TimeAgo delay="500">{this.props.time}</TimeAgo></li>
     );
   },
 
 });
 
+// Mixin para eliminar un setInterval cuando el elemento se borra
+var SetIntervalMixin = {
+
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+
+  componentWillUnmount: function() {
+    this.intervals.map(clearInterval);
+  },
+
+};
+
 // ¿Hace cuanto se envió el mensaje?
 var TimeAgo = React.createClass({
 
+  mixins: [SetIntervalMixin],
+
   render: function () {
     return (
-      <small className="time-ago">{moment(this.props.children).fromNow()}</small>
+      <small className="time-ago">{this.state.time}</small>
     );
-  }
+  },
+
+  componentDidMount: function () {
+    this.setInterval(this.updateTime, this.props.delay);
+  },
+
+  getInitialState: function () {
+    return { time: moment(this.props.children).fromNow() };
+  },
+
+  updateTime: function () {
+    this.setState({ time: moment(this.props.children).fromNow() });
+  },
 
 });
 
