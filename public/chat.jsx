6969c2f375e5f61610ce0a0c34b91dca8fc8e5f4
abalozz <alivanprox@gmail.com>
2015-04-28 22:25:06 +0200

    Componentes del chat

---
+++ public/chat.jsx
@@ -0,0 +1,54 @@
+// Contenedor del chat
+var ChatBox = React.createClass({
+
+  render: function () {
+    return (
+      <div>ChatBox</div>
+    );
+  },
+
+});
+
+// Formulario para enviar mensajes
+var ChatForm = React.createClass({
+
+  render: function () {
+    return (
+      <div>ChatForm</div>
+    );
+  },
+
+});
+
+// Listado de mensajes
+var MessageList = React.createClass({
+
+  render: function () {
+    return (
+      <div>MessageList</div>
+    );
+  },
+
+});
+
+// Cada mensaje individual
+var Message = React.createClass({
+
+  render: function () {
+    return (
+      <div>Message</div>
+    );
+  },
+
+});
+
+// ¿Hace cuanto se envió el mensaje?
+var TimeAgo = React.createClass({
+
+  render: function () {
+    return (
+      <div>TimeAgo</div>
+    );
+  }
+
+});
