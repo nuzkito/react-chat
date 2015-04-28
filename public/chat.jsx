f82d9e2738c11302bd059b3fc10e45bba6ff1e8b
abalozz <alivanprox@gmail.com>
2015-04-28 22:55:00 +0200

    Enviar y mostrar mensajes

--- public/chat.jsx
+++ public/chat.jsx
@@ -2,35 +2,51 @@
 var ChatBox = React.createClass({
 
   render: function () {
-
-    var messageList = [
-      {
-        id: 1,
-        username: 'Usuario',
-        body: 'Contenido del mensaje',
-        time: new Date(),
-      },
-      {
-        id: 2,
-        username: 'Usuario',
-        body: 'Contenido del mensaje',
-        time: new Date(),
-      },
-      {
-        id: 3,
-        username: 'Usuario',
-        body: 'Contenido del mensaje',
-        time: new Date(),
-      },
-    ];
-
     return (
       <div className="chat-box">
         <h1 className="title">Chat con ReactJS</h1>
-        <MessageList messageList={messageList} />
-        <ChatForm />
+        <MessageList messageList={this.state.messageList} />
+        <ChatForm onMessageSubmit={this.handleMessageSubmit} />
       </div>
     );
+  },
+
+  getInitialState: function () {
+    return {
+      messageList: [
+        {
+          id: 1,
+          username: 'Usuario',
+          body: 'Contenido del mensaje',
+          time: new Date(),
+        },
+        {
+          id: 2,
+          username: 'Usuario',
+          body: 'Contenido del mensaje',
+          time: new Date(),
+        },
+        {
+          id: 3,
+          username: 'Usuario',
+          body: 'Contenido del mensaje',
+          time: new Date(),
+        },
+      ],
+    }
+  },
+
+  handleMessageSubmit: function (message) {
+    this.addNewMessage(message);
+  },
+
+  addNewMessage: function (message) {
+    var messageList = this.state.messageList;
+    if (messageList.length > 100) {
+      messageList.shift();
+    }
+    messageList.push(message);
+    this.setState({ messageList: messageList });
   },
 
 });
@@ -40,12 +56,28 @@ var ChatForm = React.createClass({
 
   render: function () {
     return (
-      <form className="chat-form">
-        <input className="input username-input" type="text" placeholder="Nombre de usuario" />
-        <input className="input body-input" type="text" placeholder="¡Escribe algo! :D" />
+      <form className="chat-form" onSubmit={this.handleSubmit}>
+        <input className="input username-input" type="text" placeholder="Nombre de usuario" ref="username" />
+        <input className="input body-input" type="text" placeholder="¡Escribe algo! :D" ref="body" />
         <button className="button">Enviar</button>
       </form>
     );
+  },
+
+  handleSubmit: function (event) {
+    event.preventDefault();
+
+    var message = {
+      username: React.findDOMNode(this.refs.username).value.trim(),
+      body: React.findDOMNode(this.refs.body).value.trim(),
+    }
+
+    if (!message.username || !message.body) {
+      return;
+    }
+
+    this.props.onMessageSubmit(message);
+    React.findDOMNode(this.refs.body).value = '';
   },
 
 });
